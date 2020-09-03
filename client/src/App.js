6395132fa31a675fe4259be5953d8aeb94dbc711
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-09-03 18:08:26 +0530

    Basic setup for react app is done

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import User from './Components/user/User'
+
+function App() {
+  // https://run.mocky.io/v3/4427f590-f937-4b12-984d-48a3395d6f0b
+  return (
+    <div className="App">
+      <User/>
+    </div>
+  );
+}
+
+export default App;
