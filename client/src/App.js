283d494cd216567906517fe1c8efed08e6c2e38d
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-09-03 20:08:45 +0530

    added user card

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
-import User from './Components/user/User'
+import axios from 'axios';
+import User from './Components/user/User';
 
-function App() {
-  // https://run.mocky.io/v3/4427f590-f937-4b12-984d-48a3395d6f0b
-  return (
-    <div className="App">
-      <User/>
-    </div>
-  );
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount = () => {
+    axios
+      .get('https://run.mocky.io/v3/4427f590-f937-4b12-984d-48a3395d6f0b')
+      .then(res =>
+        this.setState({
+          data: res.data.members
+        })
+      )
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    const { data } = this.state
+    return (
+      <div className="App bg-dark" style={{height:'100vh'}}>
+        <User users={data} />
+      </div>
+    );
+  }
 }
-
-export default App;
