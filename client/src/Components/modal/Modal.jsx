d73b48ad2d58565b284091931660ff79cff2f510
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-09-04 11:01:05 +0530

    modal ui is ready with functionalities

--- client/src/Components/modal/Modal.jsx
+++ client/src/Components/modal/Modal.jsx
@@ -1,32 +1,79 @@
 import React, { Component } from 'react'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { v4 as uuidv4 } from 'uuid';
 
 export default class Modal extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            startDate: new Date(),
+            data: []
+        }
+    }
+
+    handleChange = date => {
+        const { userDetails } = this.props
+        let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Aug', 'Oct', 'Nov', 'Dec']
+        let selectedMonth = months[date.getMonth()]
+        let selectedDay = date.getDate()
+        let selectedYear = date.getFullYear()
+        let selectedDate = selectedMonth + ' ' + selectedDay + ' ' + selectedYear
+
+        const currData = userDetails && userDetails.activity_periods.filter(e => e.start_time.includes(selectedDate))
+        this.setState({
+            startDate: date,
+            data: currData
+        });
+    };
+
+    componentDidMount = () => {
+        let currentDate = new Date()
+        this.handleChange(currentDate)
+    }
+
     render() {
+        const { userDetails } = this.props
+        const { data, startDate } = this.state        
         return (
-            <>
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
-                    Launch demo modal
-                </button>
-                <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-4'>
+                        <iframe src="https://giphy.com/embed/3oz8xKaR836UJOYeOc" width="200" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+                    </div>
+                    <div className='col-8'>
+                        <div className='row'>
+                            <div className='col-12'>
+                                <h6>Name of the user : {userDetails.real_name}</h6>
+                                <hr />
+                                <div>Select a date :
+                                <DatePicker
+                                        className='ml-3'
+                                        selected={startDate}
+                                        onChange={this.handleChange}
+                                    />
+                                </div>
                             </div>
-                            <div class="modal-body">
-                                ...
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
+                            <div className='col-12'>
+                                {
+                                    data && data.length !== 0 ?
+                                        data.map(time => (
+                                            <div className="card text-white bg-dark p-1 my-3" key={uuidv4()}>
+                                                <div className="card-body">
+                                                    <p className="card-text">Start time : {time.start_time}</p>
+                                                    <p className="card-text">End time : {time.end_time}</p>
+                                                </div>
+                                            </div>
+                                        ))
+                                        :
+                                        <div className='mt-3'>Sorry no data available for selected day, Please select some other day from calendar</div>
+                                }
                             </div>
                         </div>
                     </div>
                 </div>
-            </>
+            </div>
         )
+
     }
 }
